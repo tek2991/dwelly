70f1ed6a10c84efbf0eee901dba3d7270ecdb4ea
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-09-22 07:47:27 +0530

    Added Add role UI

--- resources/views/app/user/edit.blade.php
+++ resources/views/app/user/edit.blade.php
@@ -39,7 +39,17 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
-                <h2 class="text-xl font-regular pt-2 pb-4">Assigned User Roles</h2>
+                <span class="flex justify-between items-center mt-2 mb-4">
+                    <h2 class="text-xl font-regular">Assigned User Roles</h2>
+                    <button
+                        class="inline-flex items-center px-2 py-1 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                        </svg>
+                        <span>Add Role</span>
+                    </button>
+                </span>
                 <livewire:users-roles-table :user="$user" />
             </div>
         </div>
