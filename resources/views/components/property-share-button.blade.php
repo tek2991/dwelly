31ea561b7a2c33fd91bae2f0e9c02fd1fb93bedc
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2022-10-15 08:43:02 +0530

    Updates

--- resources/views/components/property-share-button.blade.php
+++ resources/views/components/property-share-button.blade.php
@@ -11,19 +11,19 @@
     <div class="relative">
         <div x-show="dropdownOpen"
             class="absolute right-0 mt-8 w-48 bg-white rounded-md overflow-hidden shadow-xl z-20">
-            <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.dwelly.in/pages/viewProperty/{{ $property->id }}" title="Share on Facebook" class="flex px-4 py-2 text-sm text-gray-800 border-b hover:bg-blue-100">
+            <a href="https://www.facebook.com/sharer/sharer.php?u=https://www.dwelly.in/pages/viewProperty/{{ $property->id }}" title="Share on Facebook" class="flex px-4 py-2 text-sm text-gray-800 border-b hover:bg-darker">
                 <img src="{{ url('resources/icons/facebook.svg') }}" alt="" class="w-5 mr-3">
                 <span class="text-gray-600">Facebook</span>
               </a>
-              <a href="https://api.whatsapp.com/send?text=Check%20out%20this%20awesome%20property%20on%20Dwelly%20https://www.dwelly.in/pages/viewProperty/{{ $property->id }}" title="Share on Whatsapp" class="flex px-4 py-2 text-sm text-gray-800 border-b hover:bg-blue-100">
+              <a href="https://api.whatsapp.com/send?text=Check%20out%20this%20awesome%20property%20on%20Dwelly%20https://www.dwelly.in/pages/viewProperty/{{ $property->id }}" title="Share on Whatsapp" class="flex px-4 py-2 text-sm text-gray-800 border-b hover:bg-darker">
                 <img src="{{ url('resources/icons/whatsapp.svg') }}" alt="" class="w-5 mr-3">
                 <span class="text-gray-600">Whatsapp</span>
               </a>
-              <a href="mailto:?subject=I love this property on Dwelly&body=2BHK 2Bath Apartment. Here's the link https://www.dwelly.in/pages/viewProperty/{{ $property->id }}" title="Share on Email" class="flex px-4 py-2 text-sm text-gray-800 border-b hover:bg-blue-100">
+              <a href="mailto:?subject=I love this property on Dwelly&body=2BHK 2Bath Apartment. Here's the link https://www.dwelly.in/pages/viewProperty/{{ $property->id }}" title="Share on Email" class="flex px-4 py-2 text-sm text-gray-800 border-b hover:bg-darker">
                 <img src="{{ url('resources/icons/email.svg') }}" alt="" class="w-5 mr-3">
                 <span class="text-gray-600">Email</span>
               </a>
-              <a href="#" title="Download PDF" class="flex px-4 py-2 text-sm text-gray-800 border-b hover:bg-blue-100">
+              <a href="#" title="Download PDF" class="flex px-4 py-2 text-sm text-gray-800 border-b hover:bg-darker">
                 <img src="{{ url('resources/icons/pdf.svg') }}" alt="" class="w-5 mr-3">
                 <span class="text-gray-600">Brochure</span>
               </a>
