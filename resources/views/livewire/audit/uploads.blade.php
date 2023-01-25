ad58fae0e4c40774e82cb5bbb65894fd806437a5
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2023-01-25 09:07:06 +0530

    Updated btn

--- resources/views/livewire/audit/uploads.blade.php
+++ resources/views/livewire/audit/uploads.blade.php
@@ -3,9 +3,25 @@
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
             <div class="flex justify-between mb-6">
                 <h2 class="mb-4 font-semibold text-xl text-gray-800 leading-tight">Uploads</h2>
-                <x-jet-button
-                onclick="Livewire.emit('openModal', 'audit.upload-image-modal', {{ json_encode(['checklist_id' => $checklist_id]) }})"
-                class="cursor-pointer">Upload</x-jet-button>
+                <div>
+                    <x-jet-button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover"
+                        type="button">Upload<svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none"
+                            stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7">
+                            </path>
+                        </svg></x-jet-button>
+                    <!-- Dropdown menu -->
+                    <div id="dropdownHover"
+                        class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton">
+                            <li>
+                                <a href="#"
+                                    onclick="Livewire.emit('openModal', 'audit.upload-image-modal', {{ json_encode(['checklist_id' => $checklist_id]) }})"
+                                    class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Image</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
                 {{-- {{ $uploads }} --}}
