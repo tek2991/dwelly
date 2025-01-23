da1ce80bd0d51e45e72f7885266b0957056e89e9
Tumtin Eliezer Koren <eliezer.kt@gmail.com>
2025-01-23 23:44:13 +0530

    Updated

--- app/Http/Livewire/Audit/CompleteChecklist.php
+++ app/Http/Livewire/Audit/CompleteChecklist.php
@@ -18,7 +18,7 @@ class CompleteChecklist extends Component
         $this->auditChecklist = $auditChecklist;
         $this->confirm = $auditChecklist->completed;
         $this->editable = $this->auditChecklist->audit->completed == false && $this->auditChecklist->completed == false;
-        $this->next_checklist_item_exists = $this->auditChecklist->audit->auditChecklists->where('completed', false)->first();
+        $this->next_checklist_item_exists = $this->auditChecklist->audit->auditChecklists->where('completed', false)->first() !== null;
     }
 
     public function rules()
@@ -33,7 +33,7 @@ class CompleteChecklist extends Component
         $this->validate();
 
         // CHeck if auditChecklist has atleast one uploaded file
-        if($this->auditChecklist->uploads->count() == 0) {
+        if ($this->auditChecklist->uploads->count() == 0) {
             $this->err = 'Please upload atleast one file before completing the checklist.';
             return;
         }
@@ -55,8 +55,14 @@ class CompleteChecklist extends Component
     public function routeTo()
     {
 
-        if($this->next_checklist_item_exists) {
-            return redirect()->route('auditChecklist.show', $this->next_checklist_item_exists->id);
+        if ($this->next_checklist_item_exists) {
+            $next_item = $this->auditChecklist
+                ->audit
+                ->auditChecklists
+                ->where('completed', false)
+                ->skip(1)  // Skip the first item, to get the next incomplete item
+                ->first();
+            return redirect()->route('auditChecklist.show', $next_item)
         }
 
         return redirect()->route('audit.edit', $this->auditChecklist->audit->id);
